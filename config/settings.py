41b8d789b8460df18a8eefa800a5e40b8b2d10e2
SrAndrePrado <andrepradorodolfo@gmail.com>
2026-04-11 23:26:22 -0300

    fix: adiciona django e gunicorn

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=l*f-4tmgv3p2!$u0%uj4z4+2dlvb_hvrpcs^@e+uud^%%=hhu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
